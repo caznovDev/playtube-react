18ca7faa6ea8b6871b3fab0f558ec2368bff99a1
Casimiro Novela <casimirocustodionovela.ccn@gmail.com>
2025-11-19 20:49:34 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 
-export default function Navbar() {
+export default function Navbar({ onMenuToggle }) {
   return (
     <header className="navbar">
+      <button
+        className="navbar-hamburger"
+        onClick={onMenuToggle}
+      >
+        ☰
+      </button>
+
       <div className="navbar-logo">
         <span className="navbar-logo-dot" />
         <span>PlayTube</span>
@@ -11,7 +18,7 @@ export default function Navbar() {
       <div className="navbar-search">
         <input
           className="navbar-search-input"
-          placeholder="Pesquisar (não funcional ainda)"
+          placeholder="Search"
         />
       </div>
     </header>

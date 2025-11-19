920e7596d5f3d340f1c6ec8b51cbbad600ac002e
Casimiro Novela <casimirocustodionovela.ccn@gmail.com>
2025-11-19 21:06:07 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./styles.css"; // 👈 CSS global
+import "./index.css"; // 👈 Tailwind
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
